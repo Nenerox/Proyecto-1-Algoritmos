2d79bdd64df76bcbb7c7d4cf4a117a50c7f82c7a
Nenerox <andres.ps0308@gmail.com>
2026-02-10 17:05:59 -0600

    Se agregaron los metodos basicos de pila y posibles futuros

--- Stack.java
+++ Stack.java
@@ -1,3 +1,43 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.NoSuchElementException;
+
 public class Stack {
+    private Deque<byte[]> stack;
+
+    public Stack() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void push(byte[] value) {
+        stack.push(value);
+    }
+
+    public byte[] pop() {
+        try {
+            return stack.pop();
+        } catch (NoSuchElementException e) {
+            throw new RuntimeException("Error al hacer pop: la pila está vacía", e);
+        }
+    }
+
+    public byte[] peek() {
+        if (stack.peek() == null) {
+            System.out.println("Advertencia: la pila está vacía");
+        }
+        return stack.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
     
+    public void clear() {
+        stack.clear();
+    }
+
+    //Para el metodo --trace Fase 2
+    public String peekAll() {
+        return "TOP -->" + stack.iterator() + "<-- BOTTOM";
+    }
 }
