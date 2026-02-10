bcdeb5377e6b0f443bbebc58a6f864e8b73ae2b6
Nenerox <andres.ps0308@gmail.com>
2026-02-10 17:05:22 -0600

    Se agrego ToDo de los OP fase 1 y 2

--- OPcodeOperations.java
+++ OPcodeOperations.java
@@ -1,3 +1,27 @@
 public class OPcodeOperations {
+    /*Todo:
+    -Fase 1
+    OP_0/1..16
+    OP_PUSHDATA
+    OP_DUP
+    OP_DROP
+    OP_EQUAL
+    OP_EQUALVERIFY
+    OP_HASH160
+    OP_CHECKSIG
     
+    -Fase 2
+    OP_SWAP
+    OP_OVER
+    OP_NOT
+    OP_BOOLAND
+    OP_BOOLOR
+    OP_ADD
+    OP_SUB
+    OP_NUMEQUALVERIFY
+    OP_LESSTHAN
+    OP_GREATERTHAN
+    OP_LESSTHANOREQUAL
+    OP_GREATERTHANOREQUAL
+    */     
 }
