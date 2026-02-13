866ab7906fa8ac0c41576fbd7ead15373778b603
Nenerox <andres.ps0308@gmail.com>
2026-02-12 18:24:05 -0600

    Se cambio de else if a usar un hashmap para mejor expansion y control

--- demo/src/main/java/org/script/interpreter.java
+++ demo/src/main/java/org/script/interpreter.java
@@ -1,12 +1,36 @@
 package org.script;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class interpreter {
     private Stack Scriptstack;
     private OPcodeOperations operations;
+    private Map<String, OPcode> tablaOpCodes;
 
+    /*
+    Crea y asigna los objetos para guardar y llamar los diferentes metodos necesarios
+    Llama al metodo para asignar los OPcodes
+     */
     public interpreter() {
         this.Scriptstack = new Stack();
         this.operations = new OPcodeOperations();
+        this.tablaOpCodes = new HashMap<>();
+        registrarOPcodes();
+    }
+
+    /*
+    Se registran todos los metodos de OPcodes en un hash map para poder ser llamados de manera sencilla
+    */
+    private void registrarOPcodes(){
+        tablaOpCodes.put("OP_0", operations::OP_0);
+        tablaOpCodes.put("OP_1", operations::OP_1);
+        tablaOpCodes.put("OP_DUP", operations::OP_DUP);
+        tablaOpCodes.put("OP_DROP",operations::OP_DROP);
+        tablaOpCodes.put("OP_EQUAL", operations::OP_EQUAL);
+        tablaOpCodes.put("OP_EQUALVERIFY", operations::OP_EQUALVERIFY);
+        tablaOpCodes.put("OP_CHECKSIG", operations::OP_CHECKSIG);
+        tablaOpCodes.put("OP_HAS160", operations::OP_HASH160);
     }
 
     /**
@@ -25,23 +49,35 @@ public class interpreter {
         }
     }
 
-    /**
-     * Ejecuta una instrucción individual
+    /*
+     Ejecuta los metodos de Opcodes dependiendo de la instruccion actual
      */
     private void ejecutar(String inst) {
-        if (inst.equals("OP_0")) operations.OP_0(Scriptstack);
-        else if (inst.equals("OP_1")) operations.OP_1(Scriptstack);
-        else if (inst.equals("OP_DUP")) operations.OP_DUP(Scriptstack);
-        else if (inst.equals("OP_DROP")) operations.OP_DROP(Scriptstack);
-        else if (inst.equals("OP_EQUAL")) operations.OP_EQUAL(Scriptstack);
-        else if (inst.equals("OP_EQUALVERIFY")) operations.OP_EQUALVERIFY(Scriptstack);
-        else if (inst.equals("OP_HASH160")) operations.OP_HASH160(Scriptstack);
-        else if (inst.equals("OP_CHECKSIG")) operations.OP_CHECKSIG(Scriptstack);
-        else if (inst.startsWith("<") && inst.endsWith(">")) {
+        //Se maneja el ingreso de OP_2 a 16 y pushdata individualmente por su caso especial de datos
+        if (inst.startsWith("OP_")) {
+            try {
+                int value = Integer.parseInt(inst.substring(3));
+                if (value >= 2 && value <=16) {
+                    operations.OP_2_16(Scriptstack, (byte) value);
+                    return;
+                }
+            } catch (NumberFormatException  ignored) {
+            }
+        }
+
+        if (inst.startsWith("<") && inst.endsWith(">")) {
             String data = inst.substring(1, inst.length() - 1);
             operations.OP_PUSHDATA(Scriptstack, hexToBytes(data));
+            return;
         }
-        else throw new IllegalArgumentException("Instrucción desconocida: " + inst);
+
+        OPcode OP = tablaOpCodes.get(inst);
+
+        if (OP != null){
+            OP.execute(Scriptstack);
+            return;
+        }
+        throw new IllegalArgumentException("Instrucción desconocida: " + inst);
     }
 
     /**
