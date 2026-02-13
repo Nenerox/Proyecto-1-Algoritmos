6bd849bdee89a0d5e49aeafed746795cff61e75c
Jimena <mjimevm@gmail.com>
2026-02-12 20:56:45 -0600

    Remove commented-out method for processing files in TXTReader class

--- demo/src/main/java/org/script/TXTReader.java
+++ demo/src/main/java/org/script/TXTReader.java
@@ -29,33 +29,4 @@ public class TXTReader {
         // Retornar el archivo como un arreglo de strings, cada elemento es una línea del script
         return resultado.trim().split("\\s+");
     }
-/* 
-    public boolean procesarArchivo(String ruta) {
-        File archivo = new File(ruta);
-        if (!archivo.exists()) {
-            System.out.println("Error: El archivo no existe");
-            return false;
-        }
-        try {
-            System.out.println("Procesando: " + archivo.getName());
-            System.out.println();
-            String[] script = leerArchivo(archivo);
-            System.out.println("Script cargado:");
-
-            for (int i = 0; i < script.length; i++) {
-                System.out.println((i+1) + ". " + script[i]);
-            }
-            System.out.println();
-            boolean resultado = interprete.evaluateScript(script);            
-            if (resultado) {
-                System.out.println("SCRIPT VALIDO");
-            } else {
-                System.out.println("SCRIPT INVALIDO");
-            }
-            return resultado;
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            return false;
-        }
-    }*/
 }
