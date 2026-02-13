614aae4be94bba5586ea048dd929dd375f743a76
Nenerox <andres.ps0308@gmail.com>
2026-02-12 19:29:54 -0600

    Test de P2PKH con script insertado y keys generadas

--- demo/src/main/java/org/script/Main.java
+++ demo/src/main/java/org/script/Main.java
@@ -11,17 +11,24 @@ package org.script;
 
 public class Main {
     public static void main(String[] args) {
+        interpreter interpreter = new interpreter();
+
         System.out.println("╔════════════════════════════════════════╗");
         System.out.println("║  BITCOIN SCRIPT INTERPRETER - Fase 1   ║");
         System.out.println("╚════════════════════════════════════════╝\n");
-        
-        String archivo = "script.txt";
-        
-        if (args.length > 0) {
-            archivo = args[0];
-        }
-        
-        ManagerPDF manager = new ManagerPDF();
-        manager.procesarArchivo(archivo);
+
+        String[] script = {
+            "<0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101>",
+            "<020202020202020202020202020202020202020202020202020202020202020202>",
+            "OP_DUP",
+            "OP_HASH160",
+            "<A9A8D4AE65DE409A1EF6AB6608F0CE3FED019438>",
+            "OP_EQUALVERIFY",
+            "OP_CHECKSIG"
+        };
+
+        boolean resutlado = interpreter.evaluateScript(script);
+
+        System.out.print("resultado: " + resutlado);
     }
 }
