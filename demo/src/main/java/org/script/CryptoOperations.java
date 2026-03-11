a88c05fd08d4ec2c95382ac8ad5d395b8e77628b
Nenerox <andres.ps0308@gmail.com>
2026-03-11 12:14:58 -0600

    Se actualizaron los hash256 y hash 160 a el codigo previamente comentado

--- demo/src/main/java/org/script/CryptoOperations.java
+++ demo/src/main/java/org/script/CryptoOperations.java
@@ -1,23 +1,9 @@
 package org.script;
 
 import java.security.MessageDigest;
-import java.util.Arrays;
 
 public class CryptoOperations {
-    
-    // Para OP_HASH160
-    public static byte[] hash160(byte[] data) {
-        try {
-            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-            byte[] hash1 = sha256.digest(data);
-            byte[] hash2 = sha256.digest(hash1);
-            return Arrays.copyOf(hash2, 20);
-        } catch (Exception e) {
-            throw new RuntimeException("Error hash160");
-        }
-    }
 
-    /*
     public static byte[] hash256(byte[] data) {
         try {
             MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
@@ -39,7 +25,7 @@ public class CryptoOperations {
             throw new RuntimeException("Error hash160");
         }
     }
-    */
+
     // Para OP_CHECKSIG
     public static boolean checkSignature(byte[] signature, byte[] publicKey) {
         if (signature == null || publicKey == null) return false;
