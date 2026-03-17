e5f1d7152905f3f70d876a995dedd1d19df7b7dd
AlejandroSagastume <sag25257>
2026-03-17 07:51:09 -0600

    Completación de losopcodes en los TESTS

--- demo/src/test/java/org/script/OPCodesTest.java
+++ demo/src/test/java/org/script/OPCodesTest.java
@@ -1,6 +1,7 @@
 package org.script;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,17 +17,49 @@ public class OPCodesTest {
     }
 
     @Test
-    public void testOP_0() {
+    public void testOP_0_success() {
         stack.push(new byte[]{0x01});
         operations.OP_0(stack);
         assertArrayEquals(new byte[]{0x00}, stack.pop());
     }
 
     @Test
-    public void testOP_1() {
+    public void testOP_1_success() {
         stack.push(new byte[]{0x01});
         operations.OP_1(stack);
         assertArrayEquals(new byte[]{0x01}, stack.pop());
+    }
+    
+    @Test
+    public void testOP_2_16_valid() {
+        for (byte i = 2; i <= 16; i++) {
+            stack.push(new byte[]{0x01});
+            operations.OP_2_16(stack, i);
+            assertArrayEquals(new byte[]{i}, stack.pop());
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOP_2_16_invalid() { 
+        operations.OP_2_16(stack, (byte) 1);
+    }
+
+    @Test
+    public void testOP_PUSHDATA() {
+        byte[] data = new byte[]{0x01, 0x02, 0x03};
+        operations.OP_PUSHDATA(stack, data);
+        byte[] top = stack.pop();
+        assertArrayEquals("OP_PUSHDATA DEBE PUSHEAR LOS DATOS CORRECTOS", data, top);
+    }
+
+    @Test
+    public void testOP_DUP() {
+        stack.push(new byte[]{0x01, 0x02, 0x03});
+        operations.OP_DUP(stack);
+        byte[] top1 = stack.pop();
+        byte[] top2 = stack.pop();
+        assertArrayEquals("OP_DUP DEBE DUPLICAR EL TOP DEL STACK", new byte[]{0x01, 0x02, 0x03}, top1);
+        assertArrayEquals("EL SEGUNDO DEBE SER IGUAL AL PRIMERO", new byte[]{0x01, 0x02, 0x03}, top2);
     }
     
 }
